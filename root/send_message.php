dccd3e53185cf64a403c7b0380782430f82ec649
LBC19039 <19039@lbc.school.nz>
2023-09-27 06:42:00 +0000

    Contact Form Issue Fix
    I remembered hearing a while back that the reason why this happens is because of SQL injection. From a bit of googling, I found out just adding "addslashes" works well enough. Luckily, it doesn't seem to affect the message shown within the database, which is definitely a plus.

--- root/send_message.php
+++ root/send_message.php
@@ -28,10 +28,10 @@
             <?php
                 require_once "connect.php";
 
-                $FullName = $_REQUEST["fullname"];
-                $Email = $_REQUEST["email"];
-                $PhoneNumber = $_REQUEST["phone"];
-                $Message = $_REQUEST["message"];
+                $FullName = addslashes($_REQUEST["fullname"]);
+                $Email = addslashes($_REQUEST["email"]);
+                $PhoneNumber = addslashes($_REQUEST["phone"]);
+                $Message = addslashes($_REQUEST["message"]);
 
                 $sql = "INSERT INTO contact (FullName, Email, PhoneNumber, Message) VALUES ('$FullName', '$Email', '$PhoneNumber', '$Message')";
 
